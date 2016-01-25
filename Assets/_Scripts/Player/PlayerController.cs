7e896bed67eb837478a20782ce6c6b34ae7e5cc1
kuami6 <matiasskjetne@gmail.com>
2016-01-25 20:51:31 +0100

    added a new scene.

---
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	static PlayerController playerController;
+
+	void Start ()
+	{
+		if (playerController == null)
+			playerController = GetComponent<PlayerController> ();
+	}
+
+
+	void Update ()
+	{
+	
+	}
+
+	public void SnapToPoint (Vector3 point)
+	{
+		if (point == transform.position)
+			return;
+
+		transform.position = point;
+	}
+}
