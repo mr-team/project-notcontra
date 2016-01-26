273221ca6081de2ec93279d52e3722ff29a2097e
Jonas Refseth <jonas@oisann.net>
2016-01-26 01:15:21 +0100

    Prototype v1.5

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -3,17 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	
-
-	void Start ()
-	{
-		
-	}
 
 
-	void Update ()
-	{
-	
+    public bool shouldCameraFollowPlayer = true;
+
+
+	void Update () {
+        if(shouldCameraFollowPlayer)
+            Camera.main.transform.position = new Vector3(this.transform.position.x - 15f, 30f, this.transform.position.z - 15f);
 	}
 
 	public void SnapToPoint (Vector3 point)
