8425db639359217b9ff08ba23e708ae6564ee927
kuami6 <matiasskjetne@gmail.com>
2016-01-27 22:44:27 +0100

    Fleshed out the movement system a bit (clickToMove) -added different movement speeds for different player states (Prone,crouch,walk,run). -very early setup to handle animation states. -cleaned up the code a bit.
    Added new entries in the Input manager to handle the player movement states
    Moved some folders around to eliminate duplicate folder (Materials).

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -5,12 +5,15 @@ public class Player : MonoBehaviour
 {
 	static Player player;
 
+	public float runSpeed = 10;
+	public float walkSpeed = 10;
+	public float crouchSpeed = 10;
+	public float proneSpeed = 10;
 
 	void Start ()
 	{
 		if (player == null)
 			player = GetComponent<Player> ();
-	
 	}
 
 
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,16 +1,151 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(Player))]
+[RequireComponent (typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
+	public bool shouldCameraFollowPlayer = true;
+	public bool MoveWithArrows;
 
+	Player player;
+	//For the click to move function
+	NavMeshAgent navAgent;
 
-    public bool shouldCameraFollowPlayer = true;
+	Vector3 targetPosition;
 
+	bool isCrouching;
+	bool isProne;
+	bool isWalking;
+	bool isRunning;
 
-	void Update () {
-        if(shouldCameraFollowPlayer)
-            Camera.main.transform.position = new Vector3(this.transform.position.x - 15f, 30f, this.transform.position.z - 15f);
+	float moveSpeed;
+
+	void Awake ()
+	{
+		player = GetComponent<Player> ();
+		navAgent = GetComponent<NavMeshAgent> ();
+		moveSpeed = player.walkSpeed;
+	}
+
+	void Start ()
+	{
+		targetPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		if (shouldCameraFollowPlayer)
+			Camera.main.transform.position = new Vector3 (this.transform.position.x - 15f, 30f, this.transform.position.z - 15f);
+		
+		//movement
+		if (MoveWithArrows) //should the player be controlled by WASD or by clicking on the ground
+			WASDMove ();
+		else if (!MoveWithArrows)
+		{
+			ChangeState ();
+
+			if (Input.GetMouseButton (0))
+				SetTargetPosition ();
+
+			ClickToMove ();
+		}
+	}
+
+	void WASDMove ()
+	{
+		float moveX = Input.GetAxis ("Horizontal");
+		float movez = Input.GetAxis ("Vertical");
+		float Speed = player.walkSpeed;
+		float crouchSpeed = player.walkSpeed * 0.6f;
+
+		Vector3 dir = new Vector3 (moveX, 0, movez);
+
+		if (Input.GetKey (KeyCode.LeftShift))
+		{
+			Speed = crouchSpeed;
+		}
+		transform.localPosition += dir * Speed * Time.deltaTime;
+	}
+
+	void ClickToMove ()
+	{
+		if (isRunning)
+		{
+			moveSpeed = player.runSpeed;
+		}
+		if (isWalking)
+		{
+			moveSpeed = player.walkSpeed;
+		}
+		if (isCrouching)
+		{
+			moveSpeed = player.crouchSpeed;
+		}
+		if (isProne)
+		{
+			moveSpeed = player.proneSpeed;
+		}
+		 
+		navAgent.speed = moveSpeed;
+		navAgent.SetDestination (targetPosition);
+	}
+
+	void SetTargetPosition ()
+	{
+		Plane plane = new Plane (Vector3.up, transform.position);
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		float point = 0f;
+
+		if (plane.Raycast (ray, out point))
+			targetPosition = ray.GetPoint (point);	
+	}
+
+	/// <summary>
+	/// /Changes the movement state of the player*/
+	/// </summary>
+	void ChangeState ()
+	{
+		if (Input.GetButtonDown ("Crouch"))
+		{
+			isCrouching = true;
+
+			isProne = false;
+			isWalking = false;
+			isRunning = false;
+		}
+
+		if (Input.GetButtonDown ("Prone"))
+		{
+			isProne = true;
+
+			isCrouching = false;
+			isWalking = false;
+			isRunning = false;
+		}
+
+		if (Input.GetButtonDown ("Walk"))
+		{
+			isWalking = true;
+
+			isProne = false;
+			isCrouching = false;
+			isRunning = false;
+		}
+
+		if (Input.GetButtonDown ("Run"))
+		{
+			isRunning = true;
+
+			isProne = false;
+			isCrouching = false;
+			isWalking = false;
+		}	
+	}
+
+	void HandleAnimation ()
+	{
+		
 	}
 
 	public void SnapToPoint (Vector3 point)
